064fded886f139720866d446cab009d71c33a88b
Atul Kumar Pandey <pandeyatul0095@gmail.com>
2021-12-17 11:06:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,14 @@ useEffect(() => {
 
   return getmyNFTs()
 }, [])
-
+const express = require("express")
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+});
   return (
    <div className="app" >
      <Header />
