777fb7c18416ea2bbe559fa9dd8382751b8b9b7a
Atul Kumar Pandey <pandeyatul0095@gmail.com>
2021-12-17 21:41:29 +0530

    cors change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const [punkListData, setPunkListData] =useState([])
 const[selectedPunk, setSelectedPunk] =useState(0)
 useEffect(() => {
   const getmyNFTs = async () => {
-    const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x4D35f7FAC082f89F7D8A5bD6E5F7Cbf332E4eE72&order_direction=asc'
+    const openseaData = await axios.get('https://cors-anywhere.herokuapp.com/https://testnets-api.opensea.io/assets?asset_contract_address=0x4D35f7FAC082f89F7D8A5bD6E5F7Cbf332E4eE72&order_direction=asc'
     )
     console.log(openseaData.data.assets)
     setPunkListData(openseaData.data.assets)
@@ -21,14 +21,6 @@ useEffect(() => {
 
   return getmyNFTs()
 }, [])
-const express = require("express")
-var app = express();
-var server = app.listen(3000);
-var io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-});
   return (
    <div className="app" >
      <Header />
